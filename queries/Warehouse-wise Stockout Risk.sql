3e205831509af1f109dd724523d50d006662ba36
rakeshkpradhan15-hub <rakeshkpradhan15@gmail.com>
2026-06-02 15:33:00 +0530

    Delete queries/Warehouse-wise Stockout Risk.sql

--- queries/Warehouse-wise Stockout Risk.sql
+++
@@ -1,9 +0,0 @@
--- Warehouse-wise Stockout Risk
-
-SELECT
-`Warehouse Location`,
-COUNT(*) AS Stockout_Count
-FROM inventory_data
-WHERE Stock_Status = 'Stockout Risk'
-GROUP BY `Warehouse Location`
-ORDER BY Stockout_Count DESC;
